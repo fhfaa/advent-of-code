252bc64426d8ab7ef77f7cbfb9d82189022b1b30
fhfaa <flo@hunn.at>
2020-12-14 22:35:06 +0100

    Add day 14

---
+++ 2020/levels/15.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const lvl = __filename.replace(/.*?[\\\/]/g, '').replace(/[\D]/g, '');
+let input = fs.readFileSync(`${__dirname}/../input/${lvl}.txt`, 'utf8');
+
+/* *
+input = ``;
+/* */
+
+// pt.1
+const part1 = imput.split('\n');
+console.log('Part 1: ', 'TODO');
