fa45b8e610bca82848690f89f46d25dcdad1992b
Flo Z <flo@hunn.at>
2021-12-06 11:44:32 +0100

    Add day 6

--- 2021/levels/6.js
+++ 2021/levels/6.js
@@ -3,16 +3,25 @@ const lvl = __filename.replace(/.*?[\\\/]/g, '').replace(/[\D]/g, '');
 let input = fs.readFileSync(`${__dirname}/../input/${lvl}.txt`, 'utf8').replace(/\r/, '');
 
 /* *
-input = ``;
+input = `3,4,3,1,2`;
 /* */
 
-input = input.split('\n');
+// Count occurences per timer number. array index = timer value
+input = input.split(',').reduce((ret, n) => {
+  ret[+n]++;
+  return ret;
+}, Array(9).fill(0));
 
-// pt.1
-const part1 = 'TODO';
-console.log('Part 1: ', part1);
-
-
-// pt.2
-const part2 = 'TODO';
-console.log('Part 2: ', part2);
+// pt.1 + 2
+for (let i = 0; i < 256; i++) {
+  // Shift out the 0s, effectively reducing all others by one,
+  // Then add the 0s back as 6s and create new 8s
+  const was0 = input.shift();
+  input[8] = was0;
+  input[6] += was0;
+  if (i === 79) {
+    console.log('Part 1: ', Object.values(input).reduce((a, b) => a + +b, 0))
+  }
+}
+// wow such bigint
+console.log('Part 2: ', Object.values(input).reduce((a, b) => a + +b, 0));
