fa5b1a2cec6fd5c2e4296fbfdef6660d23b9d9f0
Flo Z <flo@hunn.at>
2021-12-07 08:44:06 +0100

    Add day 7

--- 2021/levels/7.js
+++ 2021/levels/7.js
@@ -3,16 +3,30 @@ const lvl = __filename.replace(/.*?[\\\/]/g, '').replace(/[\D]/g, '');
 let input = fs.readFileSync(`${__dirname}/../input/${lvl}.txt`, 'utf8').replace(/\r/, '');
 
 /* *
-input = ``;
+input = `16,1,2,0,4,2,7,1,2,14`;
 /* */
+input = input.split(',').map(parseFloat).sort((a, b) => a - b);
 
-input = input.split('\n');
 
 // pt.1
-const part1 = 'TODO';
+const median = input[Math.floor(input.length / 2)];
+const part1 = input.reduce((ret, n) => ret + Math.abs(n - median), 0);
 console.log('Part 1: ', part1);
 
 
 // pt.2
-const part2 = 'TODO';
-console.log('Part 2: ', part2);
+function fuelCost(from, to) {
+  const dist = Math.abs(from - to);
+  // sum of 1..n is n*(n+1)/2
+  return dist * (dist + 1) / 2;
+}
+
+let minCost = +Infinity;
+// for each point between smallest and highest crab position
+for (let pos = input[0], last = input[input.length - 1]; pos <= last; pos++) {
+  // sum up all fuel costs, remember only the smallest sum
+  const totalCost = input.reduce((sum, crabPos) => sum + fuelCost(crabPos, pos), 0);
+  minCost = Math.min(minCost, totalCost);
+}
+
+console.log('Part 2: ', minCost);
