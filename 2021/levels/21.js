34b3f7b5d5a86ce8fb3afecb6b2a621c2544fcb3
Flo Z <flo@hunn.at>
2021-12-22 19:35:43 +0100

    Prep 20-22

---
+++ 2021/levels/21.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const lvl = __filename.replace(/.*?[\\\/]/g, '').replace(/[\D]/g, '');
+let input = fs.readFileSync(`${__dirname}/../input/${lvl}.txt`, 'utf8').replace(/\r/, '');
+
+/* */
+input = `Player 1 starting position: 4
+Player 2 starting position: 8`;
+/* */
+
+input = input.split('\n');
+
+// pt.1
+const part1 = 'TODO';
+console.log('Part 1: ', part1);
+
+
+// pt.2
+const part2 = 'TODO';
+console.log('Part 2: ', part2);
