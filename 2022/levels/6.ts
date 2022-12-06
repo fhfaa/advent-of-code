9aead4d25c5e725f9e3b2ce037d003cc37351df7
Flo Z <flo@hunn.at>
2022-12-06 08:51:31 +0100

    Add day 6

---
+++ 2022/levels/6.ts
@@ -0,0 +1,87 @@
+import { Puzzle } from "../../puzzle";
+
+const MSG_LEN_1 = 4;
+const MSG_LEN_2 = 14;
+
+export const P = new Puzzle({
+  year: 2022,
+  day: 6,
+
+
+  part1: (input: string, isTest: boolean) => {
+    for (let i = MSG_LEN_1 - 1; i < input.length; i++) {
+      if ( // 🤭
+        input[i] !== input[i-1] &&
+        input[i] !== input[i-2] &&
+        input[i] !== input[i-3] &&
+        input[i-1] !== input[i-2] &&
+        input[i-1] !== input[i-3] &&
+        input[i-2] !== input[i-3]
+      ) {
+        return i + 1;
+      }
+    }
+  },
+
+
+  part2: (input: string, isTest: boolean) => {
+    for (let i = MSG_LEN_2; i < input.length; i++) {
+      const substr = input.substring(i - MSG_LEN_2, i);
+
+      // Convert to char array, sort and check for duplicates
+      if (substr.split('').sort().every((elem, idx, arr) => {
+        return arr.indexOf(elem) === idx;
+      })) {
+        return i;
+      }
+    }
+  },
+
+
+  tests: [{
+    name: 'Part 1 example 1',
+    part: 1,
+    expected: 7,
+    input: 'mjqjpqmgbljsphdztnvjfqwrcgsmlb',
+  }, {
+    name: 'Part 1 example 2',
+    part: 1,
+    expected: 5,
+    input: 'bvwbjplbgvbhsrlpgdmjqwftvncz',
+  }, {
+    name: 'Part 1 example 3',
+    part: 1,
+    expected: 10,
+    input: 'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg',
+  }, {
+    name: 'Part 1 example 4',
+    part: 1,
+    expected: 11,
+    input: 'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw',
+  }, {
+    name: 'Part 2 example 1',
+    part: 2,
+    expected: 19,
+    input: 'mjqjpqmgbljsphdztnvjfqwrcgsmlb'
+  }, {
+    name: 'Part 2 example 2',
+    part: 2,
+    expected: 23,
+    input: 'bvwbjplbgvbhsrlpgdmjqwftvncz'
+  }, {
+    name: 'Part 2 example 3',
+    part: 2,
+    expected: 23,
+    input: 'nppdvjthqldpwncqszvftbrmjlhg'
+  }, {
+    name: 'Part 2 example 4',
+    part: 2,
+    expected: 29,
+    input: 'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg'
+  }, {
+    name: 'Part 2 example 5',
+    part: 2,
+    expected: 26,
+    input: 'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw'
+  }]
+});
